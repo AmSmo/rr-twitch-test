5efb9bfc9ecebf9f2f427b9fc7be4413ced2aa35
AmSmo <adam.smolenski@gmail.com>
2021-01-08 12:26:36 -0500

    twitch options disable controls

--- src/Test1.js
+++ src/Test1.js
@@ -4,7 +4,16 @@ import ReactPlayer from 'react-player'
 function Test1(props){
 
     return(
-        <ReactPlayer url={"https://www.twitch.tv/riorecordszoom"} />
+        <ReactPlayer 
+            url={"https://www.twitch.tv/5uppp"} 
+            
+            playing={true}
+            muted={false}
+            config={{
+                twitch: {
+                    options: { controls: false}
+                }}}
+        />
     )
 }
 
